957e32a90478578bbff9f0c4658b6c4b79af6cf0
trungtv <tranviettrung92@gmail.com>
2019-09-11 23:19:22 +0700

    add vue ant design

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,12 +23,14 @@ export default {
   ** Global CSS
   */
   css: [
+    'ant-design-vue/dist/antd.css',
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/bus'
+    '~/plugins/bus',
+    '@/plugins/antd-ui',
   ],
   /*
   ** Nuxt.js dev-modules
